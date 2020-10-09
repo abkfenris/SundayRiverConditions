307faed5027e4737ef92ca0b6af1642bac49708f
Alex Kerney <abk@mac.com>
2020-10-09 18:45:47 -0400

    Update sunday_river_status.py

--- sunday_river_status.py
+++ sunday_river_status.py
@@ -1,26 +1,14 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import requests
 import pandas as pd
-
-
-# In[10]:
 
 
 DESKTOP_SUNDAY_RIVER_CONDITIONS_URL = "https://vicomap-cdn.resorts-interactive.com/api/details/1422"
 MOBILE_SUNDAY_RIVER_CONDITIONS_URL = "http://spotlio-snowconditions.s3.amazonaws.com/sundayriver/status.json"
 
+# Load the data from S3
 response = requests.get(MOBILE_SUNDAY_RIVER_CONDITIONS_URL)
-
 json = response.json()
-
-
-# In[48]:
-
 
 def flatten_item(item):
     """ Returns a flattened version of the item """
@@ -39,15 +27,11 @@ def flatten_item(item):
             
         else:
             output[key] = obj
-        
-    
     
     return output
 
 
-# In[49]:
-
-
+# Create Pandas DataFrames for both trails and lifts
 trails_df = pd.DataFrame([flatten_item(item) 
                        for item in json 
                        if "type" in item 
@@ -57,17 +41,10 @@ lifts_df = pd.DataFrame([flatten_item(item)
                       if "type" in item 
                       and item["type"] == "lift"])
 
-
-# In[50]:
-
-
+# Sort and write trail CSV
 trails_df = trails_df.sort_values("name")
 trails_df.to_csv("trails.csv", index=False)
 
-
-# In[51]:
-
-
+# Sort and write lift CSV
 lifts_df = lifts_df.sort_values("name")
 lifts_df.to_csv("lifts.csv", index=False)
-
